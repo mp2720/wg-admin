2b16db49e31ac45d706da22b607a251570fcddf9
mp2720 <you@example.com>
2025-02-03 10:02:47 +0300

    add user service

---
+++ wg-admin/app/user/service.go
@@ -0,0 +1,161 @@
+package user
+
+import (
+	"context"
+	"mp2720/wg-admin/wg-admin/storage"
+	"mp2720/wg-admin/wg-admin/storage/data"
+	"mp2720/wg-admin/wg-admin/transaction"
+	"mp2720/wg-admin/wg-admin/utils"
+	"time"
+
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+)
+
+type RegisterRequest struct {
+	Name         string
+	IsAdmin      bool
+	PrivateKey   *wgtypes.Key
+	Fare         string
+	MaxAddresses int64
+}
+
+type UpdateRequest struct {
+	Name         *string
+	IsAdmin      *bool
+	IsBanned     *bool
+	PrivateKey   *wgtypes.Key
+	Fare         *string
+	MaxAddresses *int64
+}
+
+type Service interface {
+	Register(ctx context.Context, req RegisterRequest) (data.User, error)
+
+	Get(ctx context.Context, name string) (data.User, error)
+
+	GetAll(ctx context.Context) ([]data.User, error)
+
+	Update(ctx context.Context, name string, req UpdateRequest) (data.User, error)
+
+	SetLastSeenNow(ctx context.Context, name string) error
+
+	SetTokenIssuedNow(ctx context.Context, name string) error
+
+	MakePayment(ctx context.Context, name string, byTime time.Time) error
+
+	DesyncUnpaid(ctx context.Context) error
+}
+
+type service struct {
+	userRepo    storage.UserRepo
+	addressRepo storage.AddressRepo
+	tm          transaction.Manager
+	clock       utils.Clock
+	// desyncNotifier desync.Notifier
+}
+
+func NewUserService(
+	userRepo storage.UserRepo,
+	addressRepo storage.AddressRepo,
+	tm transaction.Manager,
+	clock utils.Clock,
+) Service {
+	return &service{userRepo, addressRepo, tm, clock}
+}
+
+func (s *service) Register(ctx context.Context, req RegisterRequest) (data.User, error) {
+	user, err := data.NewUser(
+		req.Name,
+		req.IsAdmin,
+		req.PrivateKey,
+		req.Fare,
+		req.MaxAddresses,
+	)
+	if err != nil {
+		return data.User{}, err
+	}
+
+	if err = s.userRepo.Create(ctx, &user); err != nil {
+		return data.User{}, err
+	}
+
+	return user, nil
+}
+
+func (s *service) Get(ctx context.Context, name string) (data.User, error) {
+	return s.userRepo.GetByName(ctx, name)
+}
+
+func (s *service) GetAll(ctx context.Context) ([]data.User, error) {
+	return s.userRepo.GetAll(ctx)
+}
+
+func (s *service) Update(ctx context.Context, name string, req UpdateRequest) (data.User, error) {
+	var user data.User
+	err := s.tm.InTransaction(ctx, func(ctx context.Context) error {
+		var err error
+
+		user, err = s.userRepo.GetByNameLocked(ctx, name)
+		if err != nil {
+			return err
+		}
+
+		err = user.Update(data.UserPatch{
+			Name:         req.Name,
+			IsAdmin:      req.IsAdmin,
+			IsBanned:     req.IsBanned,
+			PrivateKey:   req.PrivateKey,
+			Fare:         req.Fare,
+			MaxAddresses: req.MaxAddresses,
+		})
+		if err != nil {
+			return err
+		}
+
+		if err = s.userRepo.Save(ctx, user); err != nil {
+			return err
+		}
+
+		if req.IsBanned != nil || req.PrivateKey != nil {
+			panic("TODO: implement")
+			// s.addressRepo.MarkOwnedByDesynced()
+			// s.desyncNotifier.Notify()
+		}
+
+		return nil
+	})
+	return user, err
+}
+
+func (s *service) update(ctx context.Context, name string, patch data.UserPatch) error {
+	return s.tm.InTransaction(ctx, func(ctx context.Context) error {
+		user, err := s.userRepo.GetByNameLocked(ctx, name)
+		if err != nil {
+			return err
+		}
+
+		if err = user.Update(patch); err != nil {
+			return err
+		}
+
+		return s.userRepo.Save(ctx, user)
+	})
+}
+
+func (s *service) SetLastSeenNow(ctx context.Context, name string) error {
+	now := s.clock.Now()
+	return s.update(ctx, name, data.UserPatch{LastSeenAt: &now})
+}
+
+func (s *service) SetTokenIssuedNow(ctx context.Context, name string) error {
+	now := s.clock.Now()
+	return s.update(ctx, name, data.UserPatch{TokenIssuedAt: &now})
+}
+
+func (s *service) MakePayment(ctx context.Context, name string, byTime time.Time) error {
+	return s.update(ctx, name, data.UserPatch{PaidByTime: &byTime})
+}
+
+func (s *service) DesyncUnpaid(ctx context.Context) error {
+	panic("TODO: implement")
+}
